05b79c71bef965c445d9aad9fa997d4642cabb24
asessorov-iv <asessoroviv@intech.rshb.ru>
2025-11-09 03:23:09 +0300

    feat: add balancer logger

--- src/server.ts
+++ src/server.ts
@@ -1,252 +1,262 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
-import { Methods } from './constants';
-import { store, UserData, syncStore } from './store/store';
-import { isUserDataValid } from './helpers';
-import { v4 as uuid, validate as idValidate } from 'uuid';
+import { createServer, IncomingMessage, ServerResponse } from "http";
+import { Methods } from "./constants";
+import { store, UserData, syncStore } from "./store/store";
+import { isUserDataValid } from "./helpers";
+import { v4 as uuid, validate as idValidate } from "uuid";
 
-const requestListener = (req: IncomingMessage, res: ServerResponse) => {
-  const [prefix, endpoint, id] = req.url.split('/').slice(1);
-  const method = req.method;
-  let isError = false;
-  let userData: UserData;
+const createRequestListener = (port: number) => {
+  return (req: IncomingMessage, res: ServerResponse) => {
+    const [prefix, endpoint, id] = req.url.split("/").slice(1);
+    const method = req.method;
+    const endpointPath = req.url || "/";
 
-  req.setEncoding('utf-8');
-  req.on('data', (data) => {
-    try {
-      userData = JSON.parse(data);
-    } catch (error) {
-      req.emit('error', 500);
-    }
-  });
+    // Логирование запроса для проверки балансировщика
+    console.log(
+      `Worker ${process.pid} on port ${port} received ${method} ${endpointPath}`
+    );
 
-  req.on('end', () => {
-    if (!isError) {
-      if (prefix === 'api' && endpoint === 'users') {
-        if (id) {
-          switch (method) {
-            case Methods.GET:
-              if (!idValidate(id)) {
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(400);
-                res.end(
-                  JSON.stringify({
-                    code: 400,
-                    message: 'Bad request',
-                  })
-                );
-                break;
-              }
-              if (store.find((user) => user.id === id)) {
-                const user = store.find((user) => user.id === id);
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(200);
-                res.end(
-                  JSON.stringify({
-                    code: 200,
-                    data: user,
-                    message: 'OK',
-                  })
-                );
-                break;
-              } else {
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(404);
-                res.end(
-                  JSON.stringify({
-                    code: 404,
-                    message: 'Not found',
-                  })
-                );
-              }
-              break;
+    let isError = false;
+    let userData: UserData;
 
-            case Methods.PUT:
-              if (!idValidate(id)) {
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(400);
-                res.end(
-                  JSON.stringify({
-                    code: 400,
-                    message: 'Bad request',
-                  })
-                );
-                break;
-              }
-              if (store.find((user) => user.id === id)) {
-                if (isUserDataValid(userData)) {
-                  const index = store.findIndex((user) => user.id === id);
-                  const [user] = store.splice(index, 1);
-                  const newUser = Object.assign(user, userData);
-                  store.push(newUser);
-                  syncStore({ type: 'UPDATE', user: newUser });
+    req.setEncoding("utf-8");
+    req.on("data", (data) => {
+      try {
+        userData = JSON.parse(data);
+      } catch (error) {
+        req.emit("error", 500);
+      }
+    });
 
-                  res.setHeader('Content-Type', 'application/json');
-                  res.writeHead(200);
-                  res.end(
-                    JSON.stringify({
-                      code: 200,
-                      data: newUser,
-                      message: 'OK',
-                    })
-                  );
-                  break;
-                } else {
-                  res.setHeader('Content-Type', 'application/json');
+    req.on("end", () => {
+      if (!isError) {
+        if (prefix === "api" && endpoint === "users") {
+          if (id) {
+            switch (method) {
+              case Methods.GET:
+                if (!idValidate(id)) {
+                  res.setHeader("Content-Type", "application/json");
                   res.writeHead(400);
                   res.end(
                     JSON.stringify({
                       code: 400,
-                      message: 'Bad request',
+                      message: "Bad request",
                     })
                   );
                   break;
                 }
-              } else {
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(404);
-                res.end(
-                  JSON.stringify({
-                    code: 404,
-                    message: 'Not found',
-                  })
-                );
-              }
+                if (store.find((user) => user.id === id)) {
+                  const user = store.find((user) => user.id === id);
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(200);
+                  res.end(
+                    JSON.stringify({
+                      code: 200,
+                      data: user,
+                      message: "OK",
+                    })
+                  );
+                  break;
+                } else {
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(404);
+                  res.end(
+                    JSON.stringify({
+                      code: 404,
+                      message: "Not found",
+                    })
+                  );
+                }
+                break;
 
-              break;
+              case Methods.PUT:
+                if (!idValidate(id)) {
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(400);
+                  res.end(
+                    JSON.stringify({
+                      code: 400,
+                      message: "Bad request",
+                    })
+                  );
+                  break;
+                }
+                if (store.find((user) => user.id === id)) {
+                  if (isUserDataValid(userData)) {
+                    const index = store.findIndex((user) => user.id === id);
+                    const [user] = store.splice(index, 1);
+                    const newUser = Object.assign(user, userData);
+                    store.push(newUser);
+                    syncStore({ type: "UPDATE", user: newUser });
 
-            case Methods.DELETE:
-              if (!idValidate(id)) {
-                res.setHeader('Content-Type', 'application/json');
+                    res.setHeader("Content-Type", "application/json");
+                    res.writeHead(200);
+                    res.end(
+                      JSON.stringify({
+                        code: 200,
+                        data: newUser,
+                        message: "OK",
+                      })
+                    );
+                    break;
+                  } else {
+                    res.setHeader("Content-Type", "application/json");
+                    res.writeHead(400);
+                    res.end(
+                      JSON.stringify({
+                        code: 400,
+                        message: "Bad request",
+                      })
+                    );
+                    break;
+                  }
+                } else {
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(404);
+                  res.end(
+                    JSON.stringify({
+                      code: 404,
+                      message: "Not found",
+                    })
+                  );
+                }
+
+                break;
+
+              case Methods.DELETE:
+                if (!idValidate(id)) {
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(400);
+                  res.end(
+                    JSON.stringify({
+                      code: 400,
+                      message: "Bad request",
+                    })
+                  );
+                  break;
+                }
+                if (store.find((user) => user.id === id)) {
+                  const index = store.findIndex((user) => user.id === id);
+                  store.splice(index, 1);
+                  syncStore({ type: "DELETE", id });
+
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(204);
+                  res.end();
+
+                  break;
+                } else {
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(404);
+                  res.end(
+                    JSON.stringify({
+                      code: 404,
+                      message: "Not found",
+                    })
+                  );
+                }
+                break;
+
+              default:
+                res.setHeader("Content-Type", "application/json");
                 res.writeHead(400);
                 res.end(
                   JSON.stringify({
                     code: 400,
-                    message: 'Bad request',
+                    message: "Bad request",
                   })
                 );
                 break;
-              }
-              if (store.find((user) => user.id === id)) {
-                const index = store.findIndex((user) => user.id === id);
-                store.splice(index, 1);
-                syncStore({ type: 'DELETE', id });
-
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(204);
-                res.end();
-
-                break;
-              } else {
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(404);
+            }
+          } else {
+            switch (method) {
+              case Methods.GET:
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(200);
                 res.end(
                   JSON.stringify({
-                    code: 404,
-                    message: 'Not found',
+                    code: 200,
+                    data: store,
+                    message: "OK",
                   })
                 );
-              }
-              break;
+                break;
 
-            default:
-              res.setHeader('Content-Type', 'application/json');
-              res.writeHead(400);
-              res.end(
-                JSON.stringify({
-                  code: 400,
-                  message: 'Bad request',
-                })
-              );
-              break;
+              case Methods.POST:
+                if (isUserDataValid(userData)) {
+                  const user = { ...userData, id: uuid() };
+                  store.push(user);
+                  syncStore({ type: "ADD", user });
+
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(201);
+                  res.end(
+                    JSON.stringify({
+                      code: 201,
+                      data: user,
+                      message: "Created",
+                    })
+                  );
+                } else {
+                  res.setHeader("Content-Type", "application/json");
+                  res.writeHead(400);
+                  res.end(
+                    JSON.stringify({
+                      code: 400,
+                      message: "Bad request",
+                    })
+                  );
+                }
+                break;
+
+              default:
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(400);
+                res.end(
+                  JSON.stringify({
+                    code: 400,
+                    message: "Bad request",
+                  })
+                );
+                break;
+            }
           }
         } else {
-          switch (method) {
-            case Methods.GET:
-              res.setHeader('Content-Type', 'application/json');
-              res.writeHead(200);
-              res.end(
-                JSON.stringify({
-                  code: 200,
-                  data: store,
-                  message: 'OK',
-                })
-              );
-              break;
-
-            case Methods.POST:
-              if (isUserDataValid(userData)) {
-                const user = { ...userData, id: uuid() };
-                store.push(user);
-                syncStore({ type: 'ADD', user });
-
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(201);
-                res.end(
-                  JSON.stringify({
-                    code: 201,
-                    data: user,
-                    message: 'Created',
-                  })
-                );
-              } else {
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(400);
-                res.end(
-                  JSON.stringify({
-                    code: 400,
-                    message: 'Bad request',
-                  })
-                );
-              }
-              break;
-
-            default:
-              res.setHeader('Content-Type', 'application/json');
-              res.writeHead(400);
-              res.end(
-                JSON.stringify({
-                  code: 400,
-                  message: 'Bad request',
-                })
-              );
-              break;
-          }
+          res.setHeader("Content-Type", "application/json");
+          res.writeHead(404);
+          res.end(
+            JSON.stringify({
+              code: 404,
+              message: "Not found",
+            })
+          );
         }
-      } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.writeHead(404);
-        res.end(
-          JSON.stringify({
-            code: 404,
-            message: 'Not found',
-          })
-        );
       }
-    }
-  });
+    });
 
-  req.on('error', () => {
-    isError = true;
-    res.setHeader('Content-Type', 'application/json');
-    res.writeHead(500);
-    res.end(
-      JSON.stringify({
-        code: 500,
-        message: 'Internal Server Error',
-      })
-    );
-  });
+    req.on("error", () => {
+      isError = true;
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(500);
+      res.end(
+        JSON.stringify({
+          code: 500,
+          message: "Internal Server Error",
+        })
+      );
+    });
+  };
 };
 
 export const runServer = (port: number) => {
+  const requestListener = createRequestListener(port);
   const server = createServer(requestListener);
 
   server.listen(port, () => {
     console.log(`Server process ${process.pid} listen: ${port}`);
   });
 
-  server.on('error', (error) => {
-    console.log('-err1-');
+  server.on("error", (error) => {
+    console.log("-err1-");
     console.log(error);
   });
 };
